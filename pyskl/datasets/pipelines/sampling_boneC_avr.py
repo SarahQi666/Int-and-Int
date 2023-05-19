a6aab912a022ef03a846824d292c834bbcfa2ffa
SarahQi666 <113270884+SarahQi666@users.noreply.github.com>
2023-05-19 21:33:37 +0800

    Update sampling_boneC_avr.py

--- pyskl/datasets/pipelines/sampling_boneC_avr.py
+++ pyskl/datasets/pipelines/sampling_boneC_avr.py
@@ -18,7 +18,7 @@ class TimeWindowSampleFrames_boneC_avr:
     are "frame_inds", "clip_len", "frame_interval" and "num_clips".
 
     Args:
-        clip_len (int): Frames of each sampled output clip. 是n？
+        clip_len (int): Frames of each sampled output clip.
         num_clips (int): Number of clips to be sampled. Default: 1.
         seed (int): The random seed used during test time. Default: 255.
     """
@@ -42,17 +42,12 @@ class TimeWindowSampleFrames_boneC_avr:
                 warning_r0(f'Arg name: {k}; Arg value: {v}')
 
 
-
-
-
-
-
     def _boneC_avr_sv(self,num_frames,num_keypoint,num_person,real_num_person,kp):
         """Define the intensity(sv) of human movement,which decides the position of the clip.
 
         Args:
-            num_frames (int): The number of frames.（“t总”）
-            num_keypoint (int): The number of keypoints.（“N”）
+            num_frames (int): The number of frames.
+            num_keypoint (int): The number of keypoints.
             real_num_person (list(int)): The number of people in every frame.
         """
         sv=[] 
@@ -116,13 +111,7 @@ class TimeWindowSampleFrames_boneC_avr:
             sv_t=sv_t/max(1,real_num_person[t])
             sv.append(sv_t)
 
-
-
         return sv
-
-    
-
-
 
     def _get_train_clips(self, num_frames, clip_len,num_keypoint,num_person,real_num_person,kp):
         """Sample indices for motion clips.
@@ -136,8 +125,6 @@ class TimeWindowSampleFrames_boneC_avr:
         for clip_idx in range(self.num_clips):
 
             svmax_idx=sv.index(max(sv))
-
-
 
             if num_frames < clip_len:
                 inds = np.arange(0, clip_len)
@@ -169,8 +156,6 @@ class TimeWindowSampleFrames_boneC_avr:
         for clip_idx in range(self.num_clips):
 
             svmax_idx=sv.index(max(sv))
-
-
 
             if num_frames < clip_len:
                 inds = np.arange(0, clip_len)
